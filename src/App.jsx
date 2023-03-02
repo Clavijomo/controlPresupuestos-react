72656d5b5e76cf2f0fa4bb4bb87e7f47c8893d43
Jonathan Clavijo <jonathanclavijo0907@gmail.com>
2023-03-01 19:32:12 -0500

    :sparkles: Proyecto de React de presupuestos implementado con LocalStorage

---
+++ src/App.jsx
@@ -0,0 +1,120 @@
+import { useState, useEffect } from 'react'
+import Header from './components/Header'
+import Modal from './components/Modal'
+import ListadoGastos from './components/ListadoGastos'
+import {generarId} from './helpers'
+import IconoNuevoGasto from './img/nuevo-gasto.svg'
+import { object } from 'prop-types'
+
+function App() {
+    const [presupuesto, setPresupuesto] = useState(
+        Number(localStorage.getItem('presupuesto')) ?? 0    
+    )
+    const [isValidPresupuesto, setIsValidPresupuesto] = useState(false);
+    const [modal, setModal] = useState(false);    
+    const [animarModal, setAnimarModal] = useState(false);
+    const [gastos, setGastos] = useState(
+        localStorage.getItem('gastos') ? JSON.parse(localStorage.getItem('gastos')) : []
+    )
+
+    const [gastoEditar, setGastoEditar] = useState({});
+
+    useEffect(() => {
+        if(Object.keys(gastoEditar).length > 0 ) {
+            setModal(true);
+            setTimeout(() => {
+                setAnimarModal(true);
+            }, 500);
+        }
+    }, [gastoEditar])
+    
+    useEffect(() => {
+        localStorage.setItem('presupuesto', presupuesto ?? 0);
+    }, [presupuesto])
+
+    useEffect(()=>{
+        const presupuestoLS = Number(localStorage.getItem('presupuesto')) ?? 0;
+
+        if(presupuestoLS > 0){
+            setIsValidPresupuesto(true);
+        }
+    }, [])
+
+    useEffect(() =>{
+        localStorage.setItem('gastos', JSON.stringify(gastos) ?? []);
+    }, [gastos])
+
+    const handleNuevoGasto = () =>{
+        setModal(true);
+        setGastoEditar({});
+
+        setTimeout(() => {
+            setAnimarModal(true);
+        }, 500);
+    }
+
+    const guardarGasto = gasto =>{  
+        // Actualizar
+        if(gasto.id) {
+            const gastosActualizados  = gastos.map( gastoState => gastoState.id === gasto.id ? gasto : gastoState );
+            setGastos(gastosActualizados);
+            setGastoEditar({});
+        } else{
+            // Nuevo gasto 
+            gasto.fecha = Date.now();
+            gasto.id = generarId();
+            setGastos([...gastos, gasto]);
+        }        
+        setAnimarModal(false);
+        setTimeout(() => {
+            setModal(false);
+        }, 500);        
+    }
+    
+    const eliminarGasto = id => {
+        const gastosActualizados = gastos.filter( gasto => gasto.id !== id);
+        setGastos(gastosActualizados);
+    }
+
+    return (
+        <div className={modal ? 'fijar' : ''}>
+            <Header 
+                gastos={gastos}
+                presupuesto={presupuesto}
+                setPresupuesto={setPresupuesto}
+                isValidPresupuesto={isValidPresupuesto}               
+                setIsValidPresupuesto={setIsValidPresupuesto}
+            />   
+            
+            {isValidPresupuesto && (
+                <div>
+                    <main>
+                        <ListadoGastos 
+                            gastos={gastos}
+                            setGastoEditar={setGastoEditar}
+                            eliminarGasto={eliminarGasto}
+                        />
+                    </main>
+                    <div className="nuevo-gasto">
+                        <img
+                            src={IconoNuevoGasto}
+                            alt="Icono nuevo gasto"
+                            onClick={handleNuevoGasto}
+                        />
+                    </div>                                 
+                </div>
+            )}               
+
+            {modal && <Modal 
+                    setModal={setModal} 
+                    animarModal={animarModal} 
+                    setAnimarModal={setAnimarModal} 
+                    guardarGasto={guardarGasto}
+                    gastoEditar={gastoEditar} 
+                    setGastoEditar={setGastoEditar}
+                />
+            }     
+        </div>
+    )
+}
+export default App;
